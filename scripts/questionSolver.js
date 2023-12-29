a48199eb391e5e93df57654a7f12c6ff2f67ea30
Tyler <tylerdonovan1506@gmail.com>
2023-12-28 20:34:03 -0500

    Chat GPT API call function added

--- scripts/questionSolver.js
+++ scripts/questionSolver.js
@@ -1,8 +1,52 @@
+
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log("QUESTION SOLVER:", request)
     if (request.action === "solve-question"){
+        chrome.storage.local.get(request.key,function(items){
+            console.log(items)
+            questions = items[parseInt(request.key)]
+            console.log(questions)
+            questionNumber = parseInt(request.questionNumber)
+            sendAPIRequest(questions[questionNumber])
+        })
+
 
     } else if (request.action === "solve-all-questions"){
         
     }
 });
+
+
+function sendAPIRequest(questionData){
+    let content = questionData.questionText + "\nAnswer Choices:\n"
+
+    content = content + questionData.answerOptions.answers.map(i => i.text).join('\n')
+
+    content = questionData.answerOptions.matches ? content + "\nMatching Options:\n" + questionData.answerOptions.matches.map(i => i.text).join('\n') : content
+
+    console.log(content)
+
+    fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + "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"
+        },
+        body: JSON.stringify({
+            'model': 'gpt-3.5-turbo',
+            'messages': [
+                {
+                    'role':'system',
+                    'content': 'The question is a ' + questionData.questionType + ' question'
+                },
+                {
+                    'role': 'user',
+                    'content': content
+                }
+            ]
+        })
+    }).then(response => response.json()).then(data => {
+        console.log(data)
+        console.log(data['choices'][0]['message']['content'])
+    });
+}
